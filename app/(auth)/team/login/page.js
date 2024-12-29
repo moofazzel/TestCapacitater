82df8fa3755d63408b1158b568a73ef7bfa5b14c
moofazzel <mofazzel444@gmail.com>
2024-12-29 10:43:03 +0600

    comment join and team login page

--- app/(auth)/team/login/page.js
+++ app/(auth)/team/login/page.js
@@ -1,11 +1,19 @@
-import TeamMemberLoginForm from "./TeamMemberLoginForm";
+// import TeamMemberLoginForm from "./TeamMemberLoginForm";
 
-const TeamLoginMemberPage = () => {
-  return (
-    <div>
-      <TeamMemberLoginForm />
-    </div>
-  );
+// const TeamLoginMemberPage = () => {
+//   return (
+//     <div>
+//       <TeamMemberLoginForm />
+//     </div>
+//   );
+// };
+
+// export default TeamLoginMemberPage;
+
+import React from "react";
+
+const page = () => {
+  return <div>page</div>;
 };
 
-export default TeamLoginMemberPage;
+export default page;
