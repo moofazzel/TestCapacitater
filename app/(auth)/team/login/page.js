0d92c46d24ac82cb35de3178edf1b1327ef5e45b
moofazzel <mofazzel444@gmail.com>
2024-12-29 10:49:02 +0600

    uncomment the team loign page

--- app/(auth)/team/login/page.js
+++ app/(auth)/team/login/page.js
@@ -1,19 +1,11 @@
-// import TeamMemberLoginForm from "./TeamMemberLoginForm";
+import TeamMemberLoginForm from "./TeamMemberLoginForm";
 
-// const TeamLoginMemberPage = () => {
-//   return (
-//     <div>
-//       <TeamMemberLoginForm />
-//     </div>
-//   );
-// };
-
-// export default TeamLoginMemberPage;
-
-import React from "react";
-
-const page = () => {
-  return <div>page</div>;
+const TeamLoginMemberPage = () => {
+  return (
+    <div>
+      <TeamMemberLoginForm />
+    </div>
+  );
 };
 
-export default page;
+export default TeamLoginMemberPage;
